b869e3dc733a84f52f4d9d5198ef998a029b57c6
Samuel Kunst <samuel.kunst@gmail.com>
2022-05-05 17:12:58 +0200

    feat: add optional persistent state

--- demo-app/pages/index.tsx
+++ demo-app/pages/index.tsx
@@ -1,43 +1,52 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
+import axios from 'axios'
 import { Count, PrismaClient } from '@prisma/client'
 import { useEffect, useState } from 'react'
-const prisma = new PrismaClient()
-
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
+const prisma = new PrismaClient()
+
 interface propType {
-  count: Count | null
+  dbCount: Count | null
   ok: boolean
 }
 
 export const getServerSideProps = async () => {
   try {
     const count = await prisma.count.findFirst()
-    return { props: { count: count, ok: true } }
+    return { props: { dbCount: count, ok: true } }
   } catch (e) {
-    return { props: { count: null, ok: false } }
+    return { props: { dbCount: null, ok: false } }
+  }
+}
+const checkDatabaseConnection = (ok: Boolean) => {
+  if (!ok) {
+    toast.warn(
+      'No database connection could be established, falling back to local state.',
+      {
+        autoClose: 3000,
+        theme: 'dark',
+      },
+    )
   }
 }
 
+const persistCount = (id: number, count: number) =>
+  axios.post(`${process.env.NEXT_PUBLIC_API_BASE}/count`, {
+    id: id,
+    count: count,
+  })
+
 const Home: NextPage<propType> = (props) => {
-  const { count, ok } = props
-  const [val, setVal] = useState(count?.count ?? 0)
-  useEffect(() => {
-    if (!ok) {
-      toast.warn(
-        'No database connection could be established, falling back to local state.',
-        {
-          autoClose: 3000,
-          theme: 'dark',
-        },
-      )
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  const { dbCount, ok } = props
+  const [count, setCount] = useState(dbCount?.count ?? 0)
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => checkDatabaseConnection(ok), [])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -47,15 +56,15 @@ const Home: NextPage<propType> = (props) => {
         <ToastContainer />
         <h1 className={styles.title}>
           Hello from{' '}
-          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">k3s</a>!
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">K3s</a>!
         </h1>
         <h3 className={styles.code}>
-          This button has been clicked {val} times
+          This button has been clicked {count} times
         </h3>
         <p
           onClick={() => {
-            setVal(val + 1)
-            console.log('clicked')
+            setCount(count + 1)
+            ok && persistCount(dbCount?.id ?? 0, count + 1)
           }}
           className={styles.card}
         >
