9b78e9520b06cbfbf1430467284c062f8f0f554b
Samuel Kunst <samuel.kunst@gmail.com>
2022-05-11 19:24:06 +0200

    fix: don't crash demo container when db-conn fails
    We don't want to crash container if db connection cannot be made (for
    purposes of being able to run the demo container without having setup
    persistent storage  / db connections)

--- demo-app/docker/run.sh
+++ demo-app/docker/run.sh
@@ -1,5 +1,10 @@
 #!/bin/ash
+
+# we don't want to crash container if db connection cannot be made (for
+# purposes of being able to run the demo container without having setup
+# persistent storage  / db connections)
+yarn prisma db push
+
 set -e
 
-yarn prisma db push
 yarn start:prod
